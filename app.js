d664143f5621fd9b8212bd4b8076bad9fc7f7bdd
Darrin <dnagengast9@gmail.com>
2015-08-26 01:10:17 -0700

    completed initial server functionality

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = require('express').createServer();
 var io = require('socket.io')(app);
 var underscore = require('underscore');
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server listening at port ', port);
+});
+
+//server files in /public folder
+app.use(express.static(__dirname + '/public'));
+
 
 var letterPool = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'F', 'G', 'G', 'G', 'G', 'H', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L', 'L', 'L', 'L', 'M', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'P', 'Q', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'U', 'U', 'V', 'V', 'V', 'W', 'W', 'W', 'X', 'X', 'Y', 'Y', 'Y', 'Z', 'Z']
 var newGameCopy = letterPool.slice();
+// var usernames = {};
 var playerCount = 0;
 var lastPeel = false;
 
@@ -34,54 +42,43 @@ function peelToWin(pool, players) {
   return pool.length < players ? false : true;
 }
 
-
-
-app.get('/', function(req, res) {
-  //should send all files html, css, and js files to client **need to update
-  res.sendfile(__dirname + '/index.html');
-});
-
-
 io.on('connection', function(socket) {
   playerCount++;
+  var addUser = true;
 
   //send each user a unique identifier
-  socket.emit('joined', playerCount);
+  socket.emit('userId', playerCount);
 
   //once a user connects, send them 7 starting pieces
   socket.emit('joined', removePieces(letterPool, 7));
+
   //broadcast event to other players
   socket.broadcast.emit('another player has joined');
 
-  socket.on('disconnect', function() {
-    //not sure how to handle if someone leaves the game, should this affect peelToWin?
-    playerCount--;
-    io.emit('player disconnected');
-  });
-
-  io.on('peeling', function(socket) {
+  socket.on('peeling', function(socket) {
     //will end the game if peeling event is emitting when pieces < players
     if (lastPeel) {
       socket.emit('youWin');
-      io.broadcast.emit('youLose', /*show winning player's board*/ );
-    }
+      socket.broadcast.emit('youLose' /*show winning player's board*/ );
+    } else {
 
-    /*send all users 1 piece. I'm not sure if socket IO allows you to emit a unique message to all users,
-    as a workaround, I send an array of length = players, each player will grab a specific piece of the array
-    based on the unique identifier given upon original connection
-    */
+      /*send all users 1 piece. I'm not sure if socket IO allows you to emit a unique message to all users, so
+		  as a workaround, I send an array of length = players, each player will grab a specific piece of the array
+		  based on the unique identifier given upon original connection
+		  */
 
-    io.emit('peeled', removePieces(letterPool, playerCount));
+      io.emit('peeled', removePieces(letterPool, playerCount));
 
-    //if there are more players than pieces left, alert everyone of last peel!
-    if (peelToWin(letterPool, playerCount)) {
-      lastPeel = true;
-      io.emit('peelToWin', 'Next Peel Wins!!!');
+      //if there are more players than pieces left, alert everyone of last peel!
+      if (peelToWin(letterPool, playerCount)) {
+        lastPeel = true;
+        io.emit('peelToWin', 'Next Peel Wins!!!');
+      }
     }
 
   });
 
-  io.on('spliting', function(socket, incomingPiece) {
+  socket.on('spliting', function(socket, incomingPiece) {
     //send user 3 pieces and add back original piece to letter pool
     socket.emit('split', split(letterPool, incomingPiece));
 
@@ -89,6 +86,14 @@ io.on('connection', function(socket) {
     if (lastPeel && !peelToWin(letterPool, playerCount)) {
       lastPeel = false;
     }
+  });
+
+  socket.on('disconnect', function() {
+    //not sure how to handle if someone leaves the game, should this affect peelToWin?
+    if (addUser) {
+      playerCount--;
+    }
+    socket.broadcast.emit('player disconnected');
   });
 
 });
